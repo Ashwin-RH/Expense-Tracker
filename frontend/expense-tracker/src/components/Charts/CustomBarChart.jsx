b5787d16f90597a7bb225481df1697accdb19158
Ashwin-RH <ashwinharagi@gmail.com>
2025-06-14 16:11:16 +0530

    Update CustomBarChart.jsx

--- frontend/expense-tracker/src/components/Charts/CustomBarChart.jsx
+++ frontend/expense-tracker/src/components/Charts/CustomBarChart.jsx
@@ -21,7 +21,7 @@ const CustomBarChart = ({data}) => {
     const CustomTooltip=({active,payload}) => {
         if(active && payload && payload.length){
             return(
-                <div className='bg-white shadow-md rounded-lg p-2 border border-gray-300'>
+                <div className='bg-white shadow-md rounded-lg p-2 border border-gray-100'>
                     <p className='text-xs font-semibold text-purple-800 mb-1'></p>
                     <p className='text-sm text-gray-600'>
                         Amount:<span className='text-sm font-medium text-gray-900'>₹{payload[0].payload.amount}</span>
