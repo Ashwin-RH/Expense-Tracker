91316cef92dc37a2aa8bcf74c61f1746d4858b79
Ashwin-RH <ashwinharagi@gmail.com>
2025-06-14 15:52:18 +0530

    Update AuthLayout.jsx

--- frontend/expense-tracker/src/components/layouts/AuthLayout.jsx
+++ frontend/expense-tracker/src/components/layouts/AuthLayout.jsx
@@ -14,19 +14,59 @@ const AuthLayout=({ children}) => {
         {children}
         <div className="text-center text-xs text-gray-500 relative -bottom-6 ">
           &copy; 2025 Ashwin Haragi. All rights reserved.
-
+          <div className=" flex items-center justify-center gap-3 mt-1 text-xs">
+          <div className="relative group inline-flex items-center">
           <a
             href="https://github.com/Ashwin-RH"
+            aria-label="Visit my GitHub"
             target="_blank"   //📌 Opens the link in a new browser tab.💡 Useful when linking to external sites so users don't leave your app.
             rel="noopener noreferrer" //Prevents new tab from controlling this page (noopener)
                                       //Also prevents the referrer URL (your site) from being sent to the target site.(noreferrer)
-            className="flex justify-center items-center gap-2 text-xs mt-1 hover:text-gray-800 transition"
+            className="flex  items-center  hover:text-gray-800 transition"
             >
               <FaGithub/>
-              github.com/Ashwin-RH
-            </a>
+              </a>
+              <span className="absolute right-full mr-3 hidden group-hover:inline bg-gray-200 text-gray-700 text-xs px-4 py-1 pb-1 shadow-inner shadow-purple-400/60 rounded whitespace-nowrap">
+                  GitHub Profile
+                </span>
+              </div>
+            
+            <div className="relative group inline-flex items-center">
+              <a
+                href="https://x.com/Ashwinharagi?t=6FuxoisznsSOeaY1j5zYdA&s=09"
+                aria-label="Follow me on X"
+                target="_blank"   //📌 Opens the link in a new browser tab.💡 Useful when linking to external sites so users don't leave your app.
+                rel="noopener noreferrer" //Prevents new tab from controlling this page (noopener)
+                                          //Also prevents the referrer URL (your site) from being sent to the target site.(noreferrer)
+                className="flex items-center hover:text-gray-800 transition"
+              >
+                <FaXTwitter/>
+                </a>
+                <span className="absolute  bottom-full mb-6 left-2 -translate-x-1/2 hidden group-hover:inline bg-gray-200 text-gray-700 text-xs px-4 py-1 pb-1  shadow-inner shadow-purple-400/60 rounded whitespace-nowrap"
+                          >
+                  Follow me on X
+                </span>
+              </div>
+
+            <div className="relative group inline-flex items-center">
+              <a
+            href="https://mail.google.com/mail/?view=cm&fs=1&to=ashwinharagi@gmail.com"
+            target="_blank"   //📌 Opens the link in a new browser tab.💡 Useful when linking to external sites so users don't leave your app.
+            rel="noopener noreferrer" //Prevents new tab from controlling this page (noopener)
+                                      //Also prevents the referrer URL (your site) from being sent to the target site.(noreferrer)
+            className="flex items-center hover:text-gray-800 transition"
+            >
+              <HiOutlineMail size={17} />
+              </a>
+              <span className="absolute  left-full ml-3  hidden group-hover:inline bg-gray-200 text-gray-700 text-xs px-4 py-1 pb-1  shadow-inner shadow-purple-400/60 rounded whitespace-nowrap"
+                          >
+                  Email Me
+                </span>
+              </div>
+              </div>
+            
         </div>
-       </div> 
+        </div>
 
      {/* Right side: Decorative background image  */}
     <div className="hidden md:block w-[40vw] h-screen bg-violet-100 bg-auth-bg-img bg-no-repeat bg-center overflow-hidden p-8 relative shadow-md shadow-violet-500/40">
