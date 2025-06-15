56069df28927af692490a43d52b3843a29fabb2f
Ashwin-RH <ashwinharagi@gmail.com>
2025-06-15 17:32:18 +0530

    Update InfoCard.jsx

--- frontend/expense-tracker/src/components/Cards/InfoCard.jsx
+++ frontend/expense-tracker/src/components/Cards/InfoCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const InfoCard = ({icon,label,value,color}) => {
-  return <div className='flex gap-6 bg-white/30 p-6 rounded-2xl shadow-md shadow-gray-300 border border-gray-200/50 backdrop-blur-md hover:shadow-xl transition'>
+  return <div className='flex gap-6 bg-white/50 mt-1 p-6 rounded-2xl shadow-md shadow-black/40 border border-gray-200/50  hover:shadow-xl transition'>
   <div className={`w-14 h-14 flex items-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl`}>
     {icon}
     </div>
     <div>
       <h6 className='text-sm text-gray-500 mb-1'>{label}</h6>
-      <span className='text-[22px]'>₹{value}</span>
+      <span className='text-[22px]'>₹{value.toLocaleString('en-IN')}</span>
     </div>
     </div>;
   };
