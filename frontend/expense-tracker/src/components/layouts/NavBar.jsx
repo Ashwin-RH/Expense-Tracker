af8067870e6e28363f5bad39a34e89aded3f067b
Ashwin-RH <ashwinharagi@gmail.com>
2025-06-15 17:25:38 +0530

    Update NavBar.jsx

--- frontend/expense-tracker/src/components/layouts/NavBar.jsx
+++ frontend/expense-tracker/src/components/layouts/NavBar.jsx
@@ -5,7 +5,7 @@ import SideMenu from './SideMenu';
 const NavBar = ({activeMenu}) => {
     const [openSideMenu, setOpenSideMenu]=useState(false);
   return (
-    <div className='flex gap-5 bg-white border border-b border-gray-300/50 backdrop-blur-[2px] py-4 px-7 sticky top-0 z-30'>
+    <div className='flex gap-5 bg-gray-400/50 shadow-md border border-b border-gray-300/50 backdrop-blur-[2px] py-4 px-7 sticky top-0 z-30'>
         <button
             className='block lg:hidden text-black cursor-pointer'
             onClick={() => setOpenSideMenu(!openSideMenu)}
