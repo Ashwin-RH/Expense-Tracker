a0ae9714a4385772602fc084dbeecc6798b95f3c
Ashwin-RH <ashwinharagi@gmail.com>
2025-06-16 09:17:37 +0530

    Update SideMenu.jsx

--- frontend/expense-tracker/src/components/layouts/SideMenu.jsx
+++ frontend/expense-tracker/src/components/layouts/SideMenu.jsx
@@ -28,9 +28,14 @@ const SideMenu = ({ activeMenu }) => {
   };
 
   return (
-    <div className='w-64 h-[calc(100vh-61px)] bg-gray-400/50 border-r border-gray-200/50 p-5 shadow-xl shadow-gray-400 sticky top-[61px] z-20 flex flex-col justify-between backdrop-blur-[2px]'>
+    <div className='w-63 h-[calc(100vh-61px)] bg-gray-800 border-r border-gray-400/50 p-5 shadow-xl shadow sticky top-[61px] z-20 flex flex-col justify-between backdrop-blur-[2px]'>
 
       <div>
+        <div className='flex justify-end'>
+      <div className='text-white text-xl p-2 border-1 border-transparent rounded-full hover:scale-105 duration-200 transition-transform cursor-pointer'>
+        <LuSettings />
+      </div>
+      </div>
       <div className='flex flex-col items-center justify-center gap-3 mt-3 mb-7'>
 
         {user?.profileImageUrl && !imgError ? ( // ✅ show image only if no error
@@ -38,7 +43,7 @@ const SideMenu = ({ activeMenu }) => {
             src={user.profileImageUrl}
             alt="Profile Image"
             onError={() => setImgError(true)} // ✅ catch broken image
-            className='w-23 h-23 bg-slate-400 rounded-full border-4 border-white shadow-md object-cover hover:shadow-xl shadow-gray-400/60 cursor-pointer'
+            className='w-23 h-23 bg-slate-400 rounded-full border-2 border-white/20 shadow-md hover:shadow-lg hover:scale-105 duration-500 shadow-gray-600/30 cursor-pointer transition-transform'
           />
         ) : (
           <CharAvatar
@@ -49,7 +54,7 @@ const SideMenu = ({ activeMenu }) => {
           />
         )}
 
-        <h5 className='text-gray-950 font-medium leading-6'>
+        <h5 className='text-white font-medium leading-6'>
           {user?.fullName || ""}
         </h5>
       </div>
@@ -58,12 +63,12 @@ const SideMenu = ({ activeMenu }) => {
   <button
     key={`menu_${index}`}
     className={`w-full flex items-center gap-4 text-[15px] py-3 px-6 rounded-lg mb-3 
-      shadow-md shadow-gray-500/60 border border-b border-gray-200/50 cursor-pointer 
-      hover:shadow-xl transition 
+      shadow-md shadow-gray-200/10 border border-b border-gray-100/10 cursor-pointer 
+      hover:scale-105 duration-300 transition-transform
       ${
         activeMenu === item.label
           ? 'border-1 border-purple-500 text-white bg-purple-500'
-          : 'bg-white/50'
+          : 'bg-gray-800/10 text-white'
       }`}
     onClick={() => handleClick(item.path)}
   >
@@ -74,7 +79,7 @@ const SideMenu = ({ activeMenu }) => {
       </div>
 
       
-      <div className='text-center text-sm text-gray-500/70 px-4 pb-4 cursor-pointer hover:text-gray-700'>
+      <div className='text-center text-sm text-gray-400/70 px-4 pb-4 cursor-pointer hover:text-white/70'>
       &copy;  Ashwin Haragi
       
     </div>
