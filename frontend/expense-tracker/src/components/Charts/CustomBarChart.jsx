93c737af86e7691ddfc13d26aba7746d48062a35
Ashwin-RH <ashwinharagi@gmail.com>
2025-06-16 09:26:27 +0530

    Update CustomBarChart.jsx

--- frontend/expense-tracker/src/components/Charts/CustomBarChart.jsx
+++ frontend/expense-tracker/src/components/Charts/CustomBarChart.jsx
@@ -15,16 +15,16 @@ const CustomBarChart = ({data}) => {
 
     //Function to alternate colors
     const getBarColor=(index)=>{
-        return index % 2===0 ? "#875cf5":"#cfbefb";
+        return index % 2===0 ? "#c95a0a":"#ba0ac9";
     };
 
     const CustomTooltip=({active,payload}) => {
         if(active && payload && payload.length){
             return(
-                <div className='bg-white shadow-md rounded-lg p-2 border border-gray-300'>
+                <div className='bg-black/40 shadow-md rounded-lg p-2 border border-gray-300 backdrop-blur-[12px]'>
                     <p className='text-xs font-semibold text-purple-800 mb-1'></p>
-                    <p className='text-sm text-gray-600'>
-                        Amount:<span className='text-sm font-medium text-gray-900'>₹{payload[0].payload.amount}</span>
+                    <p className='text-sm text-white'>
+                        Amount :<span className='text-sm font-medium text-white'> ₹ {payload[0].payload.amount}</span>
                     </p>
                 </div>
             );
@@ -32,13 +32,13 @@ const CustomBarChart = ({data}) => {
         return null;
     };
   return (
-    <div className='bg-white mt-6'>
+    <div className='bg-transparent mt-6'>
         <ResponsiveContainer width="100%" height={300}>
             <BarChart data={data}>
                 <CartesianGrid stroke="none"/>
 
-                <XAxis dataKey="month" tick={{fontSize:12, fill: "#555"}} stroke="none"/>
-                <YAxis tick={{fontSize: 12,fill: "#555"}} stroke="none" />
+                <XAxis dataKey="month" tick={{fontSize:12, fill: "white"}} stroke="none"/>
+                <YAxis tick={{fontSize: 12,fill: "white"}} stroke="none" />
 
                 <Tooltip content={CustomTooltip}/>
 
