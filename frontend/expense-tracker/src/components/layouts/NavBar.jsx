c4bf92e3e7bc037346dfe0ce4c15efaa8093d797
Ashwin-RH <ashwinharagi@gmail.com>
2025-06-16 09:14:20 +0530

    Update NavBar.jsx

--- frontend/expense-tracker/src/components/layouts/NavBar.jsx
+++ frontend/expense-tracker/src/components/layouts/NavBar.jsx
@@ -5,19 +5,19 @@ import SideMenu from './SideMenu';
 const NavBar = ({activeMenu}) => {
     const [openSideMenu, setOpenSideMenu]=useState(false);
   return (
-    <div className='flex gap-5 bg-gray-400/50 shadow-md border border-b border-gray-300/50 backdrop-blur-[2px] py-4 px-7 sticky top-0 z-30'>
+    <div className='flex gap-5 bg-black/30 shadow-md border border-b border-gray-900 backdrop-blur-[8px] py-4 px-7 sticky top-0 z-30'>
         <button
-            className='block lg:hidden text-black cursor-pointer'
+            className='block lg:hidden text-white hover:scale-105 duration-300 cursor-pointer'
             onClick={() => setOpenSideMenu(!openSideMenu)}
             >
             {openSideMenu?(
-                <HiOutlineX className='text-2xl'/>
+                <HiOutlineX className='text-2xl '/>
             ):(
-                <HiOutlineMenu className='text-2xl'/>
+                <HiOutlineMenu className='text-2xl '/>
             )}
         </button>
 
-        <h2 className='text-lg font-medium text-black'>Stash Up</h2>
+        <h2 className='text-lg font-medium text-white'>Expense Tracker</h2>
 
         {openSideMenu && (
             <div className='fixed top-[61px] -ml-4 bg-white'>
