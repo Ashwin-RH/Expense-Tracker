6292256abcd5512abb97b6ba07eb95786be5688d
Ashwin-RH <ashwinharagi@gmail.com>
2025-06-16 09:27:07 +0530

    Update CustomLineChart.jsx

--- frontend/expense-tracker/src/components/Charts/CustomLineChart.jsx
+++ frontend/expense-tracker/src/components/Charts/CustomLineChart.jsx
@@ -7,10 +7,10 @@ const CustomLineChart = ({data}) => {
     const CustomTooltip= ({active, payload}) => {
         if(active && payload && payload.length){
             return (
-                <div className='bg-white shadow-md rounded-lg p-2 border border-gray-300'>
-                    <p className='text-xs font-semibold text-purple-800 mb-1'>{payload[0].payload.category}</p>
-                    <p className='text-sm text-gray-600'>
-                        Amount: <span className='text-sm font-medium text-gray-900'>₹{payload[0].payload.amount}</span>
+                <div className='bg-black/40 shadow-md rounded-lg p-2 border border-gray-300 backdrop-blur-[2px]'>
+                    <p className='text-xs font-semibold text-white mb-1'>{payload[0].payload.category}</p>
+                    <p className='text-sm text-white'>
+                        Amount: <span className='text-sm font-medium text-white'>₹{payload[0].payload.amount}</span>
                     </p>
                 </div>
             )
@@ -18,7 +18,7 @@ const CustomLineChart = ({data}) => {
         return null;
     };
 
-  return <div className='bg-white'>
+  return <div className='bg-transparent '>
     <ResponsiveContainer width="100%" height={300}>
         <AreaChart data={data}>
         <defs>
@@ -29,8 +29,8 @@ const CustomLineChart = ({data}) => {
         </defs>
 
         <CartesianGrid stroke="none"/>
-        <XAxis dataKey="month" tick={{fontSize: 12, fill:"#555"}} stroke="none"/>
-         <YAxis  tick={{fontSize: 12, fill:"#555"}} stroke="none"/>
+        <XAxis dataKey="month" tick={{fontSize: 12, fill:"white"}} stroke="none"/>
+         <YAxis  tick={{fontSize: 12, fill:"white"}} stroke="none"/>
          <Tooltip content={<CustomTooltip />} />
 
          <Area type="monotone" dataKey="amount" stroke='#875cf5' fill="url(#incomeGradient)" strokeWidth={3} dot={{r: 3, fill: "#ab8df8"}} />
