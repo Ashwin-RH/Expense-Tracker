70c64775c36528d170090ebdb9a1fddf409349a6
Ashwin-RH <ashwinharagi@gmail.com>
2025-06-16 09:12:55 +0530

    Update TransactionInfoCard.jsx

--- frontend/expense-tracker/src/components/Cards/TransactionInfoCard.jsx
+++ frontend/expense-tracker/src/components/Cards/TransactionInfoCard.jsx
@@ -16,10 +16,10 @@ const TransactionInfoCard = ({
     onDelete,
 })=>{
     const getAmountStyles = ()=>
-        type ==="income" ? "bg-green-100 text-green-500" : "bg-red-50 text-red-500";
+        type ==="income" ? "bg-green-100/10 text-green-500" : "bg-red-100/10 text-red-500";
     
-  return <div className='group relative flex items-center gap-4 mt-2 p-3 rounded-lg hover:bg-white/40 hover:shadow-md'>
-        <div className='w-12 h-12 flex items-center justify-center text-xl text-gray-800 bg-gray-300 rounded-full hover:bg-gray-300'>
+  return <div className='group relative flex items-center gap-4 mt-2 p-3 rounded-lg hover:bg-gray-500/30 hover:shadow-md '>
+        <div className='w-12 h-12 flex items-center justify-center text-xl text-white/80 bg-gray-100/10 rounded-full hover:bg-gray-300 '>
             {icon ? (
                 <img src ={icon} alt={title} className='w-6 h-6'/>
             ):(
@@ -29,8 +29,8 @@ const TransactionInfoCard = ({
 
         <div className='flex-1 flex items-center justify-between'>
             <div>
-                <p className='text-sm text-gray-700 font-medium'>{title}</p>
-                <p className='text-xs text-gray-500 mt-1'>{date}</p>
+                <p className='text-sm text-white font-medium'>{title}</p>
+                <p className='text-xs text-gray-400 mt-1'>{date}</p>
             </div>
 
             <div className='flex items-center gap-2'>
@@ -47,7 +47,7 @@ const TransactionInfoCard = ({
                 className={`flex items-center gap-2 px-3 py-1.5 rounded-md ${getAmountStyles()}`}
                 >
                     <h6 className='text-xs font-medium'>
-                        {type === 'income' ? "+" : "-"} ₹{amount}
+                        {type === 'income' ? "+" : "-"} ₹ {amount}
                     </h6>
                     {type === "income" ? <LuTrendingUp /> : <LuTrendingDown />}
                 </div>
