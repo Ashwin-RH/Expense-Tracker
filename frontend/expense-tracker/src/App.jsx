93c3df7b22ffd6bdb409a44f72e203f4c902877f
Ashwin-RH <ashwinharagi@gmail.com>
2025-08-06 22:41:55 +0530

    settings update

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -14,7 +14,7 @@ import Expense from "./pages/Dashboard/Expense";
 import UserProvider from "./context/userContext";
 import { Toaster } from 'react-hot-toast';
 import Intro from '../src/pages/Auth/Intro';
-import Settings from "./Settings";
+import Settings from "./components/Settings";
 
 
 
